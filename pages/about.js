5757ee327dc8e48630eb5dd20f6dfe4df6e9b5df
khairul111010 <khairul18103199@gmail.com>
2022-01-20 16:01:25 +0600

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is an app to find the latest DJ and other musical events</p>
+      <p>Version 1.0</p>
+    </div>
+  );
+}
