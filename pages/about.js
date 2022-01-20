25b2ac6a26d6a55851b90144125fdd621c95d548
khairul111010 <khairul18103199@gmail.com>
2022-01-20 16:41:19 +0600

    Adding layout and dynamic title

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title="About Dj Events">
       <h2>About</h2>
       <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version 1.0</p>
-    </div>
+    </Layout>
   );
 }
